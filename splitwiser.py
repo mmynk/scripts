3a49042ba3cdda48e9fa21170d2739f7ba42793f
mmynk <mmayank@aviatrix.com>
2023-07-25 22:29:19 -0500

    Add exclusion functionality

--- splitwiser.py
+++ splitwiser.py
@@ -9,18 +9,29 @@ def main():
 	total = float(input('Enter total bill amount: '))
 	true_subtotal = input('Enter subtotal amount: ')
 	true_subtotal = float(true_subtotal) if true_subtotal else 0
-	
+
 	n = int(input('How many people are involved in the transaction? '))
 	subtotal = 0
 	breakup = {}
 	for i in range(n):
 		amounts = input(f'Enter individual item prices separated by space for person {i+1}:\n')
-		amount = sum(map(float, amounts.split(' ')))
+		amount = 0
+		if amounts:
+			amount = sum(map(float, amounts.split(' ')))
 		subtotal += amount
 		breakup[i] = {
 			'amount': amount
 		}
-	
+
+	for i in range(n):
+		amounts = input(f'Enter items to exclude from person {i+1}:\n')
+		if amounts:
+			amount = sum(map(float, amounts.split(' ')))
+			subtotal += amount
+			for j in range(n):
+				if j != i:
+					breakup[j]['amount'] += amount / (n - 1)
+
 	total_common = true_subtotal - subtotal
 	common = total_common / n
 	subtotal = true_subtotal
